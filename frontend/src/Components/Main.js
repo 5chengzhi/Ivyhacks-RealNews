ee84f87f5059ab758e3d2ce9e389bf0c225bcc1e
jack <jackbisceglia2000@gmail.com>
2020-10-03 22:56:44 -0400

    a lot of changes

--- frontend/src/Components/Main.js
+++ frontend/src/Components/Main.js
@@ -1,19 +1,27 @@
 import React, {useState, useEffect} from 'react';
 import Navbar from './Navbar';
 import Loading from './Loading';
+import Article from './Article';
+
 
 import "../Styles/main.css"
 
 function Main() {
+  const [numArticles, setNumArticles] = useState(18);
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    // SET TIMEOUT TO 3000 WHEN DONE
+    fetch("../data.json")
+      .then(res => res.json())
+      .then(data => setArticles(data.articles.slice(0,numArticles)))
     setTimeout(() => {
-      setArticles([1,2,3,4,5])
       setLoading(!loading)
+      console.log("rerun")
     }, 3000)
   },[])
+
   return (
     loading
       ?
@@ -21,10 +29,22 @@ function Main() {
       :
       <div className="container">
         <Navbar />
-        <h6>{articles[0]}</h6>
-        <h6>{articles[1]}</h6>
+        <div className="article-wrapper">
+          {articles.map(currArticle => (
+            <Article 
+              title={currArticle.Title}
+              img={currArticle.LinkToImage}
+              articleLink={currArticle.URL}  
+            />  
+          ))}
+        </div>
       </div>
   );
 }
 
 export default Main;
+
+
+// 
+
+// 
